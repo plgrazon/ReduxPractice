02815c9a549c4163905a7fb31a4de1e28c170831
Paolo Razon <plgrazon@gmail.com>
2018-08-03 15:04:22 -0700

    [ops] - Complete multiple reducers tutorial.

--- client/2-multipleReducers.js
+++ client/2-multipleReducers.js
@@ -1,7 +1,12 @@
 import { combineReducers, createStore } from 'redux';
 
-const userReducer = (state = {}, action) => {
-  let newState = Object.assign({}, state);
+const initialState = {
+  name: null,
+  age: null,
+  location: null
+}
+
+const userReducer = (state = initialState, action) => {
   switch(action.type) {
     case 'CHANGE_USER':
       state = {...state, name: action.payload}
@@ -23,6 +28,29 @@ const userReducer = (state = {}, action) => {
 }
 
 const tweetsReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_TWEET':
+      state = [
+        ...state, {
+          timeStamp: Date(),
+          tweet: action.payload.text
+      }]
+      break;
+    case 'EDIT_TWEET':
+      state = [...state];
+      let idx = action.payload.index;
+      let text = action.payload.text;
+
+      state[idx] = {
+        timeStamp: Date(),
+        tweet: text
+      }
+      break;
+    case 'DELETE_TWEET':
+      let index = action.payload.index;
+      state = [...state.slice(0, index), ...state.slice(index + 1)];
+      break;
+  }
   return state;
 }
 
@@ -40,3 +68,8 @@ store.subscribe(() => {
 store.dispatch({type: 'CHANGE_USER', payload: 'Paolo'});
 store.dispatch({type: 'CHANGE_AGE', payload: 24});
 store.dispatch({type: 'CHANGE_LOCATION', payload: 'Los Angeles'});
+
+store.dispatch({type: 'ADD_TWEET', payload: {index: null, text: 'Hello World!'}});
+store.dispatch({type: 'ADD_TWEET', payload: {index: null, text: 'I love Redux!'}});
+store.dispatch({type: 'EDIT_TWEET', payload: {index: 0, text: 'Bye World'}});
+store.dispatch({type: 'DELETE_TWEET', payload: {index: 0, text: null}});
