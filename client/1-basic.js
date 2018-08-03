127afb6f6b7b003beca8f175d55dc6d857a0614d
Paolo Razon <plgrazon@gmail.com>
2018-08-03 10:50:49 -0700

    [ops] - Basic redux implementation.

--- client/1-basic.js
+++ client/1-basic.js
@@ -1,7 +1,24 @@
 import { createStore } from 'redux';
 
-const reducer = function() {
-
+const reducer = function(state, action) {
+  if (action.type === 'INCREASE') {
+    return state + 1;
+  }
+  if (action.type === 'DECREASE') {
+    return state - 1;
+  }
+  return state;
 }
 
-const store = createStore(reducer, );
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log('store changed', store.getState());
+});
+
+store.dispatch({type: 'INCREASE', payload: 1});
+store.dispatch({type: 'INCREASE', payload: 1});
+store.dispatch({type: 'INCREASE', payload: 1});
+store.dispatch({type: 'INCREASE', payload: 1});
+store.dispatch({type: 'DECREASE', payload: 1});
+store.dispatch({type: 'DECREASE', payload: 1});
